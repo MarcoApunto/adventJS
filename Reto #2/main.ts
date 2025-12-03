f9596989196455ad17bafb03c7678ab87e5c2ac3
MarcoApunto <ferreiras.marcoa@gmail.com>
2025-12-03 13:34:15 +0100

    reto #2

--- Reto #2/main.ts
+++ Reto #2/main.ts
@@ -3,17 +3,16 @@
  * @returns {string[]} Array of manufactured gifts
  */
 function manufactureGifts(
-    giftsToProduce: Array<{ toy: string; quantity: number }>
-  ): string[] {
-    
-    const validGifts = giftsToProduce.filter(
-      (e: { toy: string; quantity: number }) =>
-        Number.isFinite(e.quantity) && e.quantity > 0
-    );
-  
-    const expanded = validGifts.map((e: { toy: string; quantity: number }) =>
-      new Array(e.quantity).fill(e.toy)
-    );
-  
-    return expanded.flat();
-  }
+  giftsToProduce: Array<{ toy: string; quantity: number }>
+): string[] {
+  const validGifts = giftsToProduce.filter(
+    (e: { toy: string; quantity: number }) =>
+      Number.isFinite(e.quantity) && e.quantity > 0
+  );
+
+  const expanded = validGifts.map((e: { toy: string; quantity: number }) =>
+    new Array(e.quantity).fill(e.toy)
+  );
+
+  return expanded.flat();
+}
